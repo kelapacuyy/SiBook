60204b00aed64caff215ec3550876d7c61c2d5da
ghifarazka <ghifarazka21@gmail.com>
2024-05-21 05:27:09 +0700

    improve cart_detail

--- backend/bookcommerce/urls.py
+++ backend/bookcommerce/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from . import views
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, add_to_cart, cart_detail, update_cart_item, delete_cart_item, delete_all_cart_items
 
 urlpatterns = [
     path('', BookListView.as_view(), name='home'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='bookcommerce-book-detail'),
-    path('add-to-cart/<int:book_id>/', views.add_to_cart, name='add_to_cart'),
-    path('cart/', views.cart_detail, name='cart_detail'),
+    path('add-to-cart/<int:book_id>/', add_to_cart, name='add_to_cart'),
+    path('cart/', cart_detail, name='cart_detail'),
+    path('cart/update/<int:item_id>/', update_cart_item, name='update_cart_item'),
+    path('cart/delete-item/<int:item_id>/', delete_cart_item, name='delete_cart_item'),
+    path('cart/delete-all/', delete_all_cart_items, name='delete_all_cart_items'),
 ]
--- backend/bookcommerce/views.py
+++ backend/bookcommerce/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, View, CreateView, UpdateView, DeleteView
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
@@ -65,4 +66,39 @@ def add_to_cart(request, book_id):
 @login_required
 def cart_detail(request):
     cart, created = Cart.objects.get_or_create(customer=request.user)
-    return render(request, 'bookcommerce/cart_detail.html', {'cart': cart})
+    cart_items = CartItem.objects.filter(cart=cart)
+    total_price = sum(item.book.price * item.quantity for item in cart_items)
+
+    return render(request, 'bookcommerce/cart_detail.html', {
+        'cart': cart,
+        'cart_items': cart_items,
+        'total_price': total_price
+    })
+
+@login_required
+@require_POST
+def update_cart_item(request, item_id):
+    action = request.POST.get('action')
+    item = get_object_or_404(CartItem, id=item_id)
+    
+    if action == 'increase':
+        item.quantity += 1
+    elif action == 'decrease' and item.quantity > 1:
+        item.quantity -= 1
+    item.save()
+    
+    return redirect('cart_detail')
+
+@login_required
+@require_POST
+def delete_cart_item(request, item_id):
+    item = get_object_or_404(CartItem, id=item_id)
+    item.delete()
+    return redirect('cart_detail')
+
+@login_required
+@require_POST
+def delete_all_cart_items(request):
+    cart = get_object_or_404(Cart, customer=request.user)
+    cart.items.all().delete()
+    return redirect('cart_detail')
