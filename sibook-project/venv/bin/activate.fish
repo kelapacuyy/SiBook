a8856932bcdb81a082ffdb02f38ca4fd1a74f642
azukagi-haru <ghifarazka21@gmail.com>
2024-06-19 10:47:22 +0700

    add requirements.txt

--- sibook-project/venv/bin/activate.fish
+++ sibook-project/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/k1nomi/Documents/other-projects/SiBook/sibook-project/venv"
+set -gx VIRTUAL_ENV "/home/ghifar/Documents/other-projects/SiBook/sibook-project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
